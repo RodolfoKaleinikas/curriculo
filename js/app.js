3d3bc35350e5a509e04d6ff986d234147ce064e8
Rodolfo Nogueira Kaleinikas <32545068+RodolfoKaleinikas@users.noreply.github.com>
2018-01-29 18:25:48 -0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,3 @@
-// Debounce 
-
-debounce = function(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
-
 
 // Animação Scroll da página
 
@@ -59,7 +42,7 @@ $('.mobile-btn').click(function() {
 // Animação do Scroll
 
 
-(function() {
+function() {
   var $target = $('[data-anime="scroll"]');
   var animationClass = 'animate';
   var offset = $(window).height() * 3/4;
@@ -79,8 +62,7 @@ $('.mobile-btn').click(function() {
 
   animeScroll();
 
-  $(document).scroll(debounce(function() {
+  $(document).scroll(function() {
     animeScroll();
-  }, 10));
-})();
+  });
 
