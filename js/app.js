a1c05a1b3603e01dba120edc372e92a9e934f138
Rodolfo Nogueira Kaleinikas <32545068+RodolfoKaleinikas@users.noreply.github.com>
2018-01-29 17:48:32 -0200

    Upload arquivos dá página

---
+++ js/app.js
@@ -0,0 +1,86 @@
+// Debounce 
+
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+
+// Animação Scroll da página
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+$('.navbar-brand, .nav-link').click(function(e) {
+  e.preventDefault();
+  var id = $(this).attr('href');
+  var targetOffset = $(id).offset().top;
+  
+  console.log(targetOffset);
+  
+  $('html, body').animate({
+    scrollTop: targetOffset
+  }, 700);
+});
+
+$('.curriculo-section').each(function() {
+  var sectionHeight = $(this).height();
+  var offsetTop = $(this).offset().top;
+  var id = $(this).attr('id');
+  var $itemMenu = $('a[href="#' + id + '"]');
+  
+  $(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+    if (offsetTop < scrollTop + 1 && offsetTop + sectionHeight > scrollTop + 1) {
+      $($itemMenu).addClass('active');
+    } else  {
+      $($itemMenu).removeClass('active');
+    }
+  })
+});
+
+// Animação botão mobile
+
+$('.mobile-btn').click(function() {
+  $(this).toggleClass('activeEx');
+});
+
+// Animação do Scroll
+
+
+(function() {
+  var $target = $('[data-anime="scroll"]');
+  var animationClass = 'animate';
+  var offset = $(window).height() * 3/4;
+
+  function animeScroll() {
+    var documentTop = $(window).scrollTop();
+
+    $target.each(function() {
+      var itemTop = $(this).offset().top;
+      if (documentTop > itemTop - offset) {
+        $(this).addClass(animationClass);
+      } else {
+        $(this).removeClass(animationClass);
+      }
+    });
+  }
+
+  animeScroll();
+
+  $(document).scroll(debounce(function() {
+    animeScroll();
+  }, 200));
+})();
+
